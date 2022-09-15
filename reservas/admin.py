796653701f90c77ec37108bcc60f2e6a559af7a3
inigo <inigogil@bilbokoeskolapioak.org>
2022-09-15 19:57:43 +0200

    Models y navbar a medias

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario, Butanito, Quemador, Tienda, Reserva
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Butanito)
+admin.site.register(Quemador)
+admin.site.register(Tienda)
+admin.site.register(Reserva)
+
--- reservas/models.py
+++ reservas/models.py
@@ -1,5 +1,9 @@
+import django.conf.global_settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import validate_email, MinLengthValidator
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 # Create your models here.
@@ -28,21 +32,22 @@ class Reserva(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=10)
     apellido = models.CharField(max_length=20)
-    email = models.CharField(validators=[validate_email])
+    email = models.CharField(max_length=100, validators=[validate_email])
     telefono = models.CharField(max_length=9, validators=[MinLengthValidator(9)])
-    objeto_reservado = models.OneToOneField(Objeto, on_delete=models.CASCADE, primary_key=False)
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
-class Usuario(models.Model):
-    email = models.CharField(validators=[validate_email])
-    nombre = models.CharField(max_length=10)
-    apellido = models.CharField(max_length=20)
+class Usuario(AbstractUser):
     telefono = models.CharField(max_length=9, validators=[MinLengthValidator(9)])
 
 
 class Observacion(models.Model):
     id = models.AutoField(primary_key=True)
-    objeto = models.OneToOneField(Objeto, on_delete=models.CASCADE, primary_key=False)
     texto = models.TextField(max_length=300)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
